ebacb3d15018b5b0d9f7a43ac18fbdd2347ed6c0
Marcel Planas <marcel@iestrategic.com>
2023-04-03 20:04:30 +0200

    Changed invite code

--- js/scripts.js
+++ js/scripts.js
@@ -214,8 +214,8 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== '54a7326c882e4f07fff45e7a46007cb6'
+            && MD5($('#invite_code').val()) !== '54a7326c882e4f07fff45e7a46007cb6') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbxfVYKLsysUzz5HUDwXPVtCu-J-fa_yXV6XptAM0wKL5ZImX08Da-2B8mPFWaNeAmyWRw/exec', data)
