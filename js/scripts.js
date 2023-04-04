c70bff5ecfa80c580693d48314ec140a5a5b7cdd
Marcel Planas <marcel@iestrategic.com>
2023-04-04 16:29:51 +0200

    Updated web app url

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbynPczKDt32mVQ01PHG4DXWujKNJkarwz5yUwNZRs3dEhkp7UOhlbxgMGQ4QXQfOMgwMg/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbx--NeBYPgPZQ-HyhMnJmvSkgu0hb9nwzs19wxQ9CYUMzmCwzR2s17SnrfnZl3R9FylHg/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
